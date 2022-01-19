726c96b4f81d4dee2a290bc641f127a5e9925888
Yagosouzag <yagomatheuscecilia@gmail.com>
2022-01-19 11:02:51 -0300

    Meu primeiro Projeto

---
+++ Projeto01.js
@@ -0,0 +1,35 @@
+const prompt = require("prompt-sync")();
+
+
+console.log(`
+Havia um guerreiro chamado Rick
+Em um belo dia ele foi desafiado a resgastar a princesa adormecida no castelo;
+Com toda sua coragem e bravura aceitou o desafio proposto e foi a caminho para resgatar a princesa!;
+Chegando la tinham alguns desafios para ele enfrentar;
+Até chegar na Princesa ele tinha 5 desafios;
+Atravessar o rio cheio de crocodilos;
+Achar as chaves do castelo;
+Derrotar o grande dragão que proteje o castelo;
+Subir todas as escadas, escapando das armadilhas;
+E por fim beijar a princesa para que ela possa acordar!;
+`)
+console.log();
+
+
+let p1 = prompt('Conseguiu atravessar o rio cheio de crocodilos?:');
+let p2 = prompt('Achou as chaves do castelo?:');
+let p3 = prompt('Derrotou o grande dragão?:');
+let p4 = prompt('Subiu todas as escadas e escapou de todas as armadilhas?:');
+let p5 = prompt('Beijou a princesa para que ela possa acordar?:');
+console.log();
+
+
+console.log('Conseguiu atravessar o rio cheio de crocodilos?:' + p1);
+console.log();
+console.log('Achou as chaves do castelo?:' + p2);
+console.log()
+console.log('Derrotou o grande dragão?:' + p3);
+console.log();
+console.log('Subiu todas as escadas e escapou de todas as armadilhas?:' + p4);
+console.log();
+console.log('Beijou a princesa para que ela possa acordar?:' + p5);
